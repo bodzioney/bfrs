df14ad263e8885a43171f5dae336e74fdf0a80f2
ethanbodzioney <etbodz@gmail.com>
2023-12-06 13:02:05 -0500

    Fixing stuff

--- src/interp.rs
+++ src/interp.rs
@@ -1,7 +1,9 @@
 use std::io::{stdin, Read};
 
+const SIZE : usize = 30_000;
+
 pub fn interp(program: &str) {
-    let mut tape: [u8; 30000] = [0; 30000];
+    let mut tape: [u8; SIZE] = [0; SIZE];
     let mut pointer = 0;
 
     let mut tbl = vec![0; program.len()];
@@ -24,24 +26,37 @@ pub fn interp(program: &str) {
     let mut i = 0;
     while i < program.len() {
         match instructions[i] as char {
-            '>' => pointer += 1,
-            '<' => pointer -= 1,
+            '>' => {
+                if pointer == SIZE - 1 {
+                    pointer = 0;
+                } else {
+                    pointer += 1;
+                }
+            }
+            '<' => {
+                if pointer == 0 {
+                    pointer = SIZE - 1;
+                } else {
+                    pointer -= 1;
+                }
+            }
             '+' => tape[pointer] = tape[pointer].wrapping_add(1),
             '-' => tape[pointer] = tape[pointer].wrapping_sub(1),
             '.' => print!("{}", tape[pointer] as char),
             '[' => {
                 if tape[pointer] == 0 {
-                    i = tbl[i]
+                    i = tbl[i];
                 }
             }
             ']' => {
                 if tape[pointer] != 0 {
-                    i = tbl[i]
+                    i = tbl[i];
                 }
             }
             ',' => {
                 let mut buf: [u8; 1] = [0];
                 stdin().read(&mut buf).unwrap();
+                tape[pointer] = buf[0];
             }
             _ => (),
         }
